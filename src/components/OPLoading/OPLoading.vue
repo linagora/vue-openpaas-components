e001f79f4c2588ee0a35a9f82dfdccf04851367e
Christophe HENRY <chenry@linagora.com>
2018-12-06 10:48:50 +0100

    Add loading component

---
+++ src/components/OPLoading/OPLoading.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="text-xs-center">
+        <v-progress-circular v-bind="componentProps"></v-progress-circular>
+        <div class="title pa-3">
+            {{$t(this.waitingText)}}
+        </div>
+    </div>
+</template>
+
+<script>
+import { VProgressCircular } from "vuetify/lib";
+
+export default VProgressCircular.extend({
+  name: "op-loading",
+  props: {
+    waitingText: { type: String, required: true }
+  },
+  computed: {
+    componentProps() {
+      const $props = Object.assign(
+        {
+          color: "amber",
+          size: 50,
+          indeterminate: true
+        },
+        this.$props
+      );
+      delete $props.waitingText;
+      return $props;
+    }
+  }
+});
+</script>
